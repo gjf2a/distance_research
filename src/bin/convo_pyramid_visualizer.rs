ed5e2686a27940350caf1103fa3f69647a597957
gjf2a <ferrer@hendrix.edu>
2021-12-02 17:00:30 -0600

    Rearchitected project to include a lib.rs file. Also began writing a visualizer for the convolutional pyramids. It is just a skeleton for Iced code at the moment.

--- src/bin/convo_pyramid_visualizer.rs
+++ src/bin/convo_pyramid_visualizer.rs
@@ -1,3 +1,4 @@
+use bare_metal_modulo::{MNum, ModNum};
 use iced::{Element, Sandbox};
 use distance_research::mnist_data::{Image, load_data_set};
 
@@ -7,22 +8,33 @@ fn main() {
 
 struct Visualizer {
     images: Vec<(u8, Image)>,
-    current: usize
+    current: ModNum<usize>
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Message {
+    Left, Right, GoTo(usize)
 }
 
 impl Sandbox for Visualizer {
-    type Message = ();
+    type Message = Message;
 
     fn new() -> Self {
-        todo!()
+        let images = load_data_set("train").unwrap();
+        let current = ModNum::new(0, images.len());
+        Visualizer { images, current }
     }
 
     fn title(&self) -> String {
-        todo!()
+        "Visualizer".to_owned()
     }
 
     fn update(&mut self, message: Self::Message) {
-        todo!()
+        match message {
+            Message::Left => self.current -= 1,
+            Message::Right => self.current += 1,
+            Message::GoTo(index) => self.current.replace(index)
+        }
     }
 
     fn view(&mut self) -> Element<'_, Self::Message> {
